8ecb308c9ec0bde01048205f5b535c842947034a
jch <pojo@foxmail.com>
2018-01-15 18:45:47 +0800

    对外接口控制器.用于东华调用打开处方录入窗口.

---
+++ rx/rx-web/src/main/java/com/rx/web/inputrx/RxOpenController.java
@@ -0,0 +1,39 @@
+package com.rx.web.inputrx;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+/**
+ * 打开处方录入窗口控制器
+ *
+ */
+@Controller
+/*@RequestMapping("/")*/
+public class RxOpenController {
+	final String RESPONSE_THYMELEAF = "thymeleaf/inputrx/";
+	final String RESPONSE_THYMELEAF_BACK = "thymeleaf/back/";
+	final String RESPONSE_JSP = "jsps/";
+
+	
+	/**
+	 * @Description 打开医生处方录入界面,对外接口 
+	 * @return 医生开处方视图(rxwin)
+	 */
+	@RequestMapping(value = "/openwin")
+	public String openWin(@RequestParam String patientId) {
+		System.out.println("patientId:"+patientId);
+		return RESPONSE_THYMELEAF + "rxwin";
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
