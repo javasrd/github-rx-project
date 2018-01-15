45f800f4acfd67e0aaf3bdb76ff7c5fb8430a0d6
jch <pojo@foxmail.com>
2018-01-15 18:45:03 +0800

    东华发送医患诊断数据包协议解释(患者实体类)

---
+++ rx/rx-dao/src/main/java/com/rx/bean/inputrx/RxPatient.java
@@ -0,0 +1,41 @@
+package com.rx.bean.inputrx;
+
+public class RxPatient {
+	private String id;  
+	private String name;
+	private String gender;
+	private String old;
+	private String cr_no;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getOld() {
+		return old;
+	}
+	public void setOld(String old) {
+		this.old = old;
+	}
+	public String getCr_no() {
+		return cr_no;
+	}
+	public void setCr_no(String cr_no) {
+		this.cr_no = cr_no;
+	}
+	
+}
