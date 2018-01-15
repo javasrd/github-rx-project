2aa8c375e6b904d6117622f72ab9206079e1b21d
jch <pojo@foxmail.com>
2018-01-15 22:38:58 +0800

    增加协议中消息常量(func 常量)

---
+++ rx/rx-dao/src/main/java/com/rx/bean/inputrx/RxAPI.java
@@ -0,0 +1,10 @@
+package com.rx.bean.inputrx;
+
+/**
+ * 消息名称(对应于协议中的func 名称)
+ * @author Administrator
+ */
+public class RxAPI {
+	static public String MSG_PATIENT_NMI="patient_nmi";  //非医保患者
+	static public String MSG_PRESC_PATIENT_NMI="presc_patient_nmi";  //非医保患者处方药品清单 
+}
